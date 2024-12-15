ec225b957dae48161aca281ea526c01cd5f361cc
n_i_p_u_n <112545976+nipunsw1999@users.noreply.github.com>
2024-12-15 17:50:36 +0530

    feedback removed

--- app.py
+++ app.py
@@ -21,7 +21,6 @@ if start:
     if uploaded_file:
         # Check the uploaded file type
         if uploaded_file.type == "application/pdf":
-            completed(f"File name: {uploaded_file.name} uploaded!", 2)
             
             # Wrap uploaded_file to BytesIO for compatibility with PyPDFLoader
             text = pdf_load(BytesIO(uploaded_file.read()), 4)
