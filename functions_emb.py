48bc449f1a6b3f60044532a709ea1aaddd769a85
n_i_p_u_n <112545976+nipunsw1999@users.noreply.github.com>
2024-12-15 10:23:09 +0530

    Embedding functions added

---
+++ functions_emb.py
@@ -0,0 +1,30 @@
+
+
+def get_pdf_content(documents):
+    raw_text = ""
+
+    for document in documents:
+        pdf_reader = PdfReader(document)
+        for page in pdf_reader.pages:
+            raw_text += page.extract_text()
+
+    return raw_text
+
+
+def get_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    text_chunks = text_splitter.split_text(text)
+    return text_chunks
+
+
+def get_embeddings(chunks):
+    embeddings = OpenAIEmbeddings()
+    # embeddings = HuggingFaceInstructEmbeddings(model_name="")
+    vector_storage = FAISS.from_texts(texts=chunks, embedding=embeddings)
+
+    return vector_storage
