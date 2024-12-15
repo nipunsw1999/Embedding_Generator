06bafa792d6fa66fa822b7fd81e1dba1e4354fbc
n_i_p_u_n <112545976+nipunsw1999@users.noreply.github.com>
2024-12-15 10:54:13 +0530

    bug fixed

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ from functions import completed, err, warn, progress_bar
 
 st.title("Embedding Generator")
 
-uploaded_file = st.file_uploader("Upload a PDF file", type="pdf")
+uploaded_file = st.file_uploader("Upload a single PDF file", type=["pdf"], accept_multiple_files=False)
 
-if uploaded_file is not None:
-    # Simulate a loading progress bar for the file upload
-    st.success("PDF uploaded successfully!")
-    progress_bar("Processing uploaded file...")
-    completed(f"File name: {uploaded_file.name}", 2)
+if uploaded_file:
+    # Check the uploaded file type
+    if uploaded_file.type == "application/pdf":
+        progress_bar("Processing uploaded file...")
+        completed(f"File name: {uploaded_file.name} uploaded!", 2)
+    else:
+        err("Invalid file type uploaded. Please upload a PDF file.", 3)
